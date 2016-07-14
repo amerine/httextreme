7ea383eb4218f1c2d8309e3857d51ae2448cd32c
Mark Turner <mark@amerine.net>
2016-07-14 10:11:39 -0700

    less verbose

--- cmd/poller/main.go
+++ cmd/poller/main.go
@@ -34,27 +34,20 @@ func main() {
 				if (maxPort - minPort) > 1 {
 					port = rand.Intn(maxPort-minPort) + minPort
 				}
-				fmt.Printf("worker-goroutine.%d port=%d", worker, port)
 
-				fmt.Printf("worker-goroutine.%d at=Dialing", worker)
 				conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", target, port), 3*time.Second)
 				if err != nil {
-					fmt.Print(err)
+					fmt.Println("Dial Failed", err, target, port)
 				}
-				fmt.Printf("worker-goroutine.%d at=FinishDialing", worker)
 
-				fmt.Printf("worker-goroutine.%d at=GET", worker)
 				fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 				all, err := ioutil.ReadAll(conn)
-				fmt.Printf("worker-goroutine.%d at=FinishGET", worker)
 				if err != nil {
-					fmt.Print(err)
+					fmt.Println("Read All Failed", err, target, port)
 				}
-				fmt.Printf("worker-goroutine.%d target=%s port=%d status=%s", worker, target, port, all)
-				fmt.Printf("worker-goroutine.%d at=Sleeping", worker)
+
 				// conn.Close()
 				time.Sleep(2000 * time.Millisecond)
-				fmt.Printf("worker-goroutine.%d at=FinishSleeping", worker)
 			}
 		}(i)
 	}
