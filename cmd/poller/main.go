12b9c24aa8bac6d3897bdc6905bfb62ce8ed9fe1
Mark Turner <mark@amerine.net>
2016-07-14 01:01:16 -0700

    misc: Log more, figure out where things are pausing

--- cmd/poller/main.go
+++ cmd/poller/main.go
@@ -33,18 +33,26 @@ func main() {
 				if (maxPort - minPort) > 1 {
 					port = rand.Intn(maxPort-minPort) + minPort
 				}
+				fmt.Printf("worker-goroutine.%d port=%d", worker, port)
+
+				fmt.Printf("worker-goroutine.%d at=Dialing", worker)
 				conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", target, port), 3*time.Second)
 				if err != nil {
 					fmt.Print(err)
 				}
+				fmt.Printf("worker-goroutine.%d at=FinishDialing", worker)
 
+				fmt.Printf("worker-goroutine.%d at=GET", worker)
 				fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 				status, err := bufio.NewReader(conn).ReadString('\n')
+				fmt.Printf("worker-goroutine.%d at=FinishGET", worker)
 				if err != nil {
 					fmt.Print(err)
 				}
 				fmt.Printf("worker-goroutine.%d target=%s port=%d status=%s", worker, target, port, status)
+				fmt.Printf("worker-goroutine.%d at=Sleeping", worker)
 				time.Sleep(1000 * time.Millisecond)
+				fmt.Printf("worker-goroutine.%d at=FinishSleeping", worker)
 			}
 		}(i)
 	}
